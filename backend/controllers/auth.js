33e8233067e9e67a61ba3d3d3fe11135e7cd1860
Jonathan Warner <jewarner57@gmail.com>
2021-07-07 21:32:28 -0400

    feat: convert auth controllers to use async await instead of promises

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,38 +2,36 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 
 // LOGIN
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
   try {
 
     const { email, password } = req.body
 
     // Find this user name
-    User.findOne({ email }, 'email password')
-      .then((user) => {
+    const user = await User.findOne({ email }, 'email password')
 
-        // If User not found
-        if (!user) {
-          return res.status(401).send({ message: 'Wrong Email or Password' });
-        }
+    // If User not found
+    if (!user) {
+      return res.status(401).send({ message: 'Wrong Email or Password' });
+    }
 
-        // Check if password matches
-        user.comparePassword(password, (err, isMatch) => {
-          if (!isMatch) {
-            // Password does not match
-            return res.status(401).send({ message: 'Wrong Email or password' });
-          }
-          // Create a token
-          const token = jwt.sign({ _id: user._id, email: user.email }, process.env.SECRET_KEY, {
-            expiresIn: '60 days',
-          });
-          // Set a cookie
-          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-          res.json({ message: 'Login Successful' })
-        });
-      })
-      .catch((err) => {
-        console.log(err);
+    // Check if password matches
+    user.comparePassword(password, (err, isMatch) => {
+
+      if (!isMatch) {
+        // If password does not match return an error
+        return res.status(401).send({ message: 'Wrong Email or password' });
+      }
+
+      // Create a token
+      const token = jwt.sign({ _id: user._id, email: user.email }, process.env.SECRET_KEY, {
+        expiresIn: '60 days',
       });
+
+      // Set a cookie
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      res.json({ message: 'Login Successful' })
+    })
 
   } catch (err) {
     return res.status(500).send({ error: err.message })
@@ -42,28 +40,30 @@ exports.login = (req, res) => {
 
 
 // SIGN UP
-exports.signup = (req, res) => {
-  // Create User
-  const newuser = new User(req.body);
+exports.signup = async (req, res) => {
+  try {
+    // Create User
+    const newuser = new User(req.body);
 
-  User.findOne({ email: newuser.email })
-    .then((user) => {
-      // If user already exists return an error
-      if (user) { return res.status(409).send({ err: 'Email already in use.' }) }
-      // else save user and send token
-      newuser
-        .save()
-        .then((user) => {
-          const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: '60 days' });
-          res.cookie('nToken', token, { maxAge: 900000 });
-          res.json({ user })
-        })
-        .catch((err) => {
-          console.log(err.message);
-          return res.status(400).send({ err });
-        });
-    })
-    .catch((err) => {
-      throw err
-    })
+    const user = await User.findOne({ email: newuser.email })
+
+    // If user already exists return an error
+    if (user) { return res.status(409).send({ err: 'Email already in use.' }) }
+
+    // Save user and send token
+    const createdUser = await newuser.save()
+
+    const token = jwt.sign({ _id: createdUser._id }, process.env.SECRET_KEY, { expiresIn: '60 days' });
+    res.cookie('nToken', token, { maxAge: 900000 });
+    res.json({ createdUser })
+  }
+  catch (err) {
+    return res.status(400).send({ err });
+  }
+}
+
+// LOGOUT
+exports.logout = (req, res) => {
+  res.clearCookie('nToken');
+  res.json({ message: 'Logout Successful' })
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,4 +10,6 @@ router.post('/login', authController.login)
 
 router.post('/signup', authController.signup)
 
+router.post('/logout', authController.logout)
+
 module.exports = router
